8057d2c795d7046d2488e67eaefd1f2db9a79ef3
kennedymutuma <santa@northpole.com>
2019-06-03 18:25:32 +0300

    import the models classes

--- app/tests.py
+++ app/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import UserProfile, Post, Comment, Location, Category, Neighborhood, Business
+from .models import *
 # Create your tests here.
 
 
